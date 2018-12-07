b4437844d1b9f98b9aa8882e01c2f52b76a0e7f2
Nandan Pandey <pandeynandancse@gmail.com>
2018-12-07 21:55:51 +0530

    Update beautyfulsoups.py

--- beautyfulsoups.py
+++ beautyfulsoups.py
@@ -1,1 +1,17 @@
+import urllib.request
+import urllib.parse
+import urllib.error
 
+from bs4 import BeautifulSoup
+
+htmls = urllib.request.urlopen('https://www.imdb.com/').read()
+soup = BeautifulSoup(htmls,'html.parser')
+tags = soup('link')
+for tag in tags:
+	x = tag.get('href',None)
+	if x == "#":
+		continue
+	elif ".html" in x:
+		continue
+	else:
+		print(x)
