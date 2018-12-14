77aa3dbf17e63fd030a0aab4545ab95c49d959da
Nandan Pandey <pandeynandancse@gmail.com>
2018-12-14 23:25:50 +0530

    Update a_href_crawl.py

--- beautifulsoups/a_href_crawl.py
+++ beautifulsoups/a_href_crawl.py
@@ -1,1 +1,16 @@
+import urllib.request
+import urllib.parse
+import urllib.error
 
+from bs4 import BeautifulSoup
+import ssl
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+htmls = urllib.request.urlopen('https://pandeynandancse.github.io/').read()
+soup = BeautifulSoup(htmls,'html.parser')
+tags = soup('a')
+for tag in tags:
+	print(tag.get('href',None))
